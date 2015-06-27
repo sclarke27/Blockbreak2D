af7dd34a8d1231c7ab457a5c06b502aae39562a9
sclarke27 <sclarke27@gmail.com>
2015-06-26 20:29:12 -0700

    0.6.26.15.01 PR ball and paddle update
    * added code which better handles the ball's overall velocity
    * paddle now accelerates and decelerates.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -11,6 +11,11 @@ public class Ball : MonoBehaviour
     public AudioSource ballDestructionSound;
     public GameObject ballBreakParticles;
 
+    private float minSpeed = 3f;
+    private float maxSpeed = 10f;
+    private ArrayList veloHistoryX = new ArrayList();
+    private ArrayList veloHistoryY = new ArrayList();
+
 
     // Use this for initialization
     void Start()
@@ -18,6 +23,9 @@ public class Ball : MonoBehaviour
         gameData = GameObject.FindObjectOfType<GameData>();
         paddle = GameObject.FindObjectOfType<Paddle>();
         paddleToBallVector = this.transform.position - paddle.transform.position;
+        veloHistoryX.Capacity = 5;
+        veloHistoryY.Capacity = 5;
+
         //Debug.Log(optionsPanel.GetUserPreference("sfxVolume"));
     }
 
@@ -49,27 +57,70 @@ public class Ball : MonoBehaviour
         }
     }
 
+    public void AddSpin(float spinAmount) {
+        if (!isLocked)
+        {
+            //Debug.Log("Spin:" + spinAmount);
+            this.rigidbody2D.AddTorque(spinAmount);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (isLocked)
+        if (!isLocked)
         {
-            this.transform.position = paddle.transform.position + paddleToBallVector;
-            if (Input.GetKeyDown(KeyCode.Space))
+            float velocityX = this.rigidbody2D.velocity.x;
+            float velocityY = this.rigidbody2D.velocity.y;
+            float currX = this.rigidbody2D.position.x;
+            float currY = this.rigidbody2D.position.y;
+            float newX = currX + velocityX;
+            float newY = currY + velocityY;
+
+            float diff = Mathf.Sqrt(Mathf.Pow((currX - newX), 2) + Mathf.Pow((currY - newY), 2));
+            if (diff > maxSpeed)
             {
-                isLocked = false;
-                this.rigidbody2D.velocity = new Vector2(0f, (gameData.ballStartingVelocity + gameData.GetDifficultyLevel()));
+                //Debug.Log("X:" + velocityX + " Y:" + velocityY + " diff:" + diff);
+                diff = diff - maxSpeed;
+                if (velocityX * 1 > 10)
+                {
+                    velocityX = (velocityX > 0) ? (Mathf.Clamp((velocityX - diff), 0, velocityX)) : (Mathf.Clamp((velocityX + diff), velocityX, 0));
+                }
+
+                if (velocityY * 1 > 10)
+                {
+                    velocityY = (velocityY > 0) ? (Mathf.Clamp((velocityY - diff), 0, velocityY)) : (Mathf.Clamp((velocityY + diff), velocityY, 0));
+                }
+
+                //Debug.Log("X:" + velocityX + " Y:" + velocityY + " diff:" + diff);
+                //Debug.Log("Too fast");
             }
+            if (diff < minSpeed)
+            {
+                diff = maxSpeed - diff;
+                if (velocityX * 1 > 10)
+                {
+                    velocityX = (velocityX > 0) ? (Mathf.Clamp((velocityX + diff), 0, velocityX)) : (Mathf.Clamp((velocityX - diff), velocityX, 0));
+                }
+
+                if (velocityY * 1 > 10)
+                {
+                    velocityY = (velocityY > 0) ? (Mathf.Clamp((velocityY + diff), 0, velocityY)) : (Mathf.Clamp((velocityY - diff), velocityY, 0));
+                }
+
+                //Debug.Log("X:" + velocityX + " Y:" + velocityY + " diff:" + diff);
+                //Debug.Log("Too fast");
+                Debug.Log("X:" + velocityX + " Y:" + velocityY + " diff:" + diff);
+                Debug.Log("Too slow");
+            }
+
+            this.rigidbody2D.velocity = new Vector2(velocityX, velocityY);
+
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                isLocked = true;
-            }
+            this.transform.position = new Vector2(paddle.transform.position.x, paddle.transform.position.y + paddleToBallVector.y);
         }
-         */
     }
 
 
@@ -79,19 +130,22 @@ public class Ball : MonoBehaviour
         //Vector2 tweak = new Vector2(0f, Random.Range(gameData.ballBounceSideVariance, gameData.ballBounceMaxVelocity + speedMultplier));
         float velocityX = this.rigidbody2D.velocity.x;
         float velocityY = this.rigidbody2D.velocity.y;
+        float currX = this.rigidbody2D.position.x;
+        float currY = this.rigidbody2D.position.y;
+        float newX = currX + velocityX;
+        float newY = currY + velocityY;
 
-        if (velocityX == 7f || velocityX == 7.9f || velocityX == -7f || velocityX == -7.9f)
-        {
-            velocityX = Mathf.Round(velocityX);
-        }
-        if (velocityY == 7f || velocityY == 7.9f || velocityY == -7f || velocityY == -7.9f)
+        float diff = Mathf.Sqrt(Mathf.Pow((currX- newX), 2) + Mathf.Pow((currY- newY), 2));
+
+        if (velocityY >= 7.4 && velocityY <= 7.6)
         {
             velocityY = Mathf.Round(velocityY);
         }
 
+        
+
         this.rigidbody2D.velocity = new Vector2(velocityX, velocityY);
 
-        //Debug.Log(this.rigidbody2D.velocity);
         if (!isLocked)
         {
             audio.volume = gameData.GetSFXVolume();
