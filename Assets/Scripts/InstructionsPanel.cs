2a29f5bad532bd5de0e8c4a502ab723e481cdef1
sclarke27 <sclarke27@gmail.com>
2015-07-07 15:56:13 -0700

    1.0.7.7.15 release
    version 1.0!

---
+++ Assets/Scripts/InstructionsPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InstructionsPanel : MonoBehaviour
+{
+
+    public Text tapToStartText;
+
+    // Use this for initialization
+    void Start()
+    {
+        if ((Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) && tapToStartText != null)
+        {
+            tapToStartText.text = "Tap here to start";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
